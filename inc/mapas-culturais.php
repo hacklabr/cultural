db76705e908eac4d8506962b3f3141197921736e
Felipe <hola@seufelipe.com.br>
2014-10-10 15:13:17 +0100

    improving the in-post events

--- inc/mapas-culturais.php
+++ inc/mapas-culturais.php
@@ -32,10 +32,19 @@ function cultural_shortcode_event( $atts ) {
         </figure>
         <div class="event-data">
             <h1 class="event__title">Histórias Contadas com Origamis <span class="event__subtitle">Com Irene Tanabe</span></h1>
-            <div class="event__venue"><strong>Local:</strong> Biblioteca Pública Marcos Rey</div>
-            <div class="event__occurrance">13 de Outubro de 2014 às 14:00</div>
-            <div class="event__classification"><strong>Classificação:</strong> Livre</div>
-            <div class="event__price"><strong>Preço:</strong> Grátis</div>
+            <div class="event__occurrances">
+                <div class="event__venue">Biblioteca Pública Marcos Rey</div>
+                <div class="event__time">13 de Outubro de 2014 às 14:00</div>
+                <a href="#" class="js-more-occurrances"><i class="fa fa-plus-circle"></i></a>
+            </div>
+            <span class="event__classification">Livre</span>
+            <div class="event__price">
+                <span class="fa-stack">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-usd fa-stack-1x fa-inverse"></i>
+                </span>
+                Grátis
+            </div>
             <a href="#" class="event__info">Mais informações</a>
         </div>
     </div>
