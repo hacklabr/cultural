cd5c82aa8cfabc004cfcf41da9ba83e51e69f4dc
Felipe <hola@seufelipe.com.br>
2014-10-22 09:32:52 +0100

    search tab

--- functions.php
+++ functions.php
@@ -93,6 +93,16 @@ add_action( 'wp_enqueue_scripts', 'cultural_scripts' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function cultural_widgets_init() {
+    register_sidebar( array (
+        'name' => __( 'Header Widget Area', 'cultural' ),
+        'description' => __( 'Appears in the header area, under the magnifying glass icon', 'cultural' ),
+        'id' => 'header-widget-area',
+        'before_widget' => '<aside id="%1$s" class="widget  %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget__title">',
+        'after_title' => '</h3>',
+    ) );
+
   	register_sidebar( array (
   		'name' => __( 'Content Widget Area', 'cultural' ),
   		'description' => '',
