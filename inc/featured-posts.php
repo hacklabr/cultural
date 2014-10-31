57361f367b675c5c9efad023858cce24373e514b
Felipe <hola@seufelipe.com.br>
2014-10-31 11:59:52 +0000

    fixing the featured posts

--- inc/featured-posts.php
+++ inc/featured-posts.php
@@ -3,14 +3,15 @@ if ( $featured_posts->have_posts() ) : ?>
     <div class="featured-posts">
         <?php while($featured_posts->have_posts()) : $featured_posts->the_post(); ?>
             <article class="feature">
-                <a href="<?php the_permalink(); ?>">
                 <?php if ( has_post_thumbnail() ) the_post_thumbnail( 'medium' ); ?>
-                <div class="overlay">
-                    <div class="feature-content">
-                        <h2 class="feature__title"><?php the_title(); ?></h2>
+                <div class="feature-content">
+                    <h2 class="feature__title"><?php the_title(); ?></h2>
+                    <div class="job__description">
+                        <p><span>A collection of essays on Seinfeld episodes</span></p>
+                        <p><span>Jekyll</span></p>
                     </div>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                 </div>
-                </a>
             </article>
         <?php endwhile; ?>
     </div><!-- /featured-posts -->
