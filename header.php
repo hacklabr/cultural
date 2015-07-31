5efda322bc8aa8298fa8c30387f870adad31a765
Gutieri Barboza <eugutierri@gmail.com>
2015-07-31 11:38:05 -0300

    remove botoes de redes sociais a baixo dos menus

--- header.php
+++ header.php
@@ -101,11 +101,6 @@
                     </div>
                 </div>
             </div>
-            
-            <div style="margin-bottom:7px;"><div class="fb-share-button" data-href="" data-layout="button_count"></div></div>
-            <div><div class="g-plus" data-action="share" data-annotation="bubble" data-href=""></div></div>
-            <div><a href="https://twitter.com/share" class="twitter-share-button" data-lang="pt">Tweetar</a></div>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         </header>
 
         <div class="main  cf">
